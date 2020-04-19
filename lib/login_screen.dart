f8a40ae9dba3c8d5100a1f3657b0546c5fd35208
Jan Sandau <j.sandau@me.com>
2020-04-19 18:33:48 +0200

    Start login_screen.dart to implement

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,32 +1,32 @@
 import 'package:eishockey_fantasy_app/about_screen.dart';
-import 'package:eishockey_fantasy_app/news_screen.dart';
 import 'package:eishockey_fantasy_app/home_screen.dart';
+import 'package:eishockey_fantasy_app/news_screen.dart';
+import 'package:eishockey_fantasy_app/registration_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class LoginScreen extends StatefulWidget{
+class LoginScreen extends StatefulWidget {
   LoginScreen({Key key}) : super(key: key);
 
   @override
   _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
 }
 
-class _MyStatefulWidgetState  extends State<LoginScreen> {
+class _MyStatefulWidgetState extends State<LoginScreen> {
   int _selectedIndex = 0;
-
 
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
-      if(_selectedIndex == 0){
-        Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen()));
-      }else if(_selectedIndex == 1){
-
-        Navigator.push(context, MaterialPageRoute(builder: (context) => AboutScreen()));
-
-      }else if(_selectedIndex == 2){
-
-        Navigator.push(context, MaterialPageRoute(builder: (context) => NewsScreen()));
+      if (_selectedIndex == 0) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => HomeScreen()));
+      } else if (_selectedIndex == 1) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => AboutScreen()));
+      } else if (_selectedIndex == 2) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => NewsScreen()));
       }
     });
   }
@@ -43,37 +43,56 @@ class _MyStatefulWidgetState  extends State<LoginScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Image.asset("assets/images/LOGO-1.png"),
+              Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const ListTile(
+                      title: Text('Login'),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'E-Mail Adresse'),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Passwort'),
+                    ),
+                    FlatButton(
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      textColor: Colors.white,
+                      color: Color.fromRGBO(157, 180, 192, 1.0),
+                      onPressed: () {
+                        print('Okay');
+                      },
+                    ),
+                    ButtonBar(
+                      children: <Widget>[
+                        FlatButton(
+                          child: const Text('Passwort vergessen?'),
+                          onPressed: () {
+                            print('Passwort vergessen');
+                            _ackAlert(context);
+                          },
+                        ),
+                        FlatButton(
+                          child: const Text('Noch kein Account?'),
+                          onPressed: () {
+                            print('Noch kein Account');
+                            Navigator.push(
+                                context, MaterialPageRoute(builder: (context) => RegistrationScreen()));
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
               //Button für die Registrierung
-              FlatButton(
-                child: Text(
-                  'Login',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                  ),
-                ),
-                textColor: Colors.white,
-                color: Color.fromRGBO(157, 180, 192, 1.0),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => HomeScreen()));
-                },
-              ),
-              FlatButton(
-                child: Text(
-                  'Registrieren',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                  ),
-                ),
-                textColor: Colors.white,
-                color: Colors.blueGrey,
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => HomeScreen()));
-                },
-              ),
             ]),
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -101,5 +120,25 @@ class _MyStatefulWidgetState  extends State<LoginScreen> {
   @override
   State<StatefulWidget> createState() {
     throw UnimplementedError();
+  }
+
+  Future<void> _ackAlert(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Na Passwort vergessen?'),
+          content: const Text('Merk dir das scheiss Passwort'),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
